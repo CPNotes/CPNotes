36b87f653d18cc7510952dc1d2194af707f8675a
CPNotes <82150839+CPNotes@users.noreply.github.com>
2021-06-14 19:06:27 +0530

    btn scroll added

--- js/cpnotes.js
+++ js/cpnotes.js
@@ -60,3 +60,13 @@ function nopopup(){
 }
 
 //back to Top
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
